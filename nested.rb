018f2a4655b673b686302247a82558ab5c9d91fe
Ha Nguyen <nkei1908@yahoo.com>
2020-01-12 00:21:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,8 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][:alans_new_info] = programmer_hash.delete :known_for
-     programmer_hash[:alans_new_info] = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash[:alans_new_info] = programmer_hash.delete :known_for
      programmer_hash
 end
 
